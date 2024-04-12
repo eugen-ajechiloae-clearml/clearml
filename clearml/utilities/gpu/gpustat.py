df3d840afdcd255ea019169cc473e9030d043e95
Eugen Ajechiloae <eugenajechiloae@gmail.com>
2024-04-12 14:24:15 +0300

    update gpustat

--- clearml/utilities/gpu/gpustat.py
+++ clearml/utilities/gpu/gpustat.py
@@ -190,17 +190,16 @@ class GPUStatCollection(object):
             def amd_query_processes():
                 num_procs = c_uint32()
                 ret = R.rocm_lib.rsmi_compute_process_info_get(None, byref(num_procs))
-                print("num procs", ret, num_procs)
                 if R.rsmi_ret_ok(ret):
                     buff_sz = num_procs.value + 10
                     proc_info = (R.rsmi_process_info_t * buff_sz)()
                     ret = R.rocm_lib.rsmi_compute_process_info_get(byref(proc_info), byref(num_procs))
-                    print("list", ret)
                     proc_info_list = (
                         [proc_info[i] for i in range(num_procs.value)]
                         if R.rsmi_ret_ok(ret)
                         else []
                     )
+                    result_proc_info_list = []
                     # query VRAM usage explicitly, as rsmi_compute_process_info_get
                     # doesn't actually return VRAM usage
                     for proc_info in proc_info_list:
@@ -208,11 +207,10 @@ class GPUStatCollection(object):
                         ret = R.rocm_lib.rsmi_compute_process_info_by_pid_get(
                             int(proc_info.process_id), byref(vram_query_proc_info)
                         )
-                        print("query vram", ret)
-                        if not R.rsmi_ret_ok(ret):
-                            return []
-                        proc_info.vram_usage = vram_query_proc_info.vram_usage
-                    return proc_info_list
+                        if R.rsmi_ret_ok(ret):
+                            proc_info.vram_usage = vram_query_proc_info.vram_usage
+                            result_proc_info_list.append(proc_info)
+                    return result_proc_info_list
                 return []
 
             def get_fan_speed():
